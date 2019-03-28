54278f955312525fd9d4ba5b49a2d0f6dae27a8a
kirito-hn <luis-n.p@live.com>
2019-03-28 20:38:24 -0300

    Update simples4.java

--- estudando/src/estudando/simples4.java
+++ estudando/src/estudando/simples4.java
@@ -1,3 +1,8 @@
+/* 4) Escreva um programa para calcular e imprimir o número de lâmpadas necessárias para iluminar um
+determinado cômodo de uma residência. Dados de entrada: a potência da lâmpada utilizada (em
+watts), as dimensões (largura e comprimento, em metros) do cômodo. Considere que a potência
+necessária é de 18 watts por metro quadrado.
+*/
 package estudando;
 import java.util.Scanner;
 import  java.lang.Math;
