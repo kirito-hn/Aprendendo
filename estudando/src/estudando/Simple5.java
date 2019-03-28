545b0ad547a016a5d2e8b26e7dcee3a46a6989c2
kirito-hn <luis-n.p@live.com>
2019-03-28 20:39:26 -0300

    Update Simple5.java

--- estudando/src/estudando/Simple5.java
+++ estudando/src/estudando/Simple5.java
@@ -1,3 +1,8 @@
+/*5) Escreva um programa para ler as dimensões de uma cozinha retangular (comprimento, largura e
+altura), calcular e escrever a quantidade de caixas de azulejos para se colocar em todas as suas
+paredes (considere que não será descontada a área ocupada por portas e janelas). Cada caixa de
+azulejos possui 1,5 m2
+*/.
 package estudando;
 
 import java.util.Scanner;
