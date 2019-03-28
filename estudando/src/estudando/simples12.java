9d93c2b1e631dc5708416c41548f7f5e519ae840
kirito-hn <luis-n.p@live.com>
2019-03-28 20:34:14 -0300

    Update simples12.java

--- estudando/src/estudando/simples12.java
+++ estudando/src/estudando/simples12.java
@@ -1,3 +1,7 @@
+/*Escreva um programa para ler uma temperatura em graus Fahrenheit, calcular e escrever o valor
+correspondente em graus Celsius.
+*/
+
 package estudando;
 import java.util.Scanner;
 public class simples12 {
